2b595e451e6696e2053679b7422ddd32b4cc390b
Ham kemboi <kemboiham3@gmail.com>
2023-06-29 02:15:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+const drivers = [
+  { name: 'John Doe', hometown: 'New York' },
+  { name: 'Jane Smith', hometown: 'Los Angeles' },
+  { name: 'Michael Johnson', hometown: 'Chicago' },
+  { name: 'Emily Davis', hometown: 'San Francisco' },
+  { name: 'David Johnson', hometown: 'Miami' }
+];
+
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
+
+// Testing the functions
+console.log(findMatching(drivers, 'john')); // Output: [{ name: 'John Doe', hometown: 'New York' }]
+console.log(fuzzyMatch(drivers, 'j')); // Output: [{ name: 'John Doe', hometown: 'New York' }, { name: 'Jane Smith', hometown: 'Los Angeles' }]
+console.log(matchName(drivers, 'Michael Johnson')); // Output: [{ name: 'Michael Johnson', hometown: 'Chicago' }]
